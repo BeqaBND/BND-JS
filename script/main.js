c66c0df69e08da03f3f2a53a989571816a72b9ba
BeqaBND <b.baindurashvili77@gmail.com>
2023-07-14 18:52:14 +0400

    added index.html and main.js

---
+++ script/main.js
@@ -0,0 +1,33 @@
+console.log("javascript");
+const num = 23;
+console.log("typeof num")
+const str = "angmar"
+console.log ("typeof str")
+const isStudent = true;
+const isDancer = false;
+console.log ("typeof isStudent, isDancer")
+const undefined1 = 15;
+let undefined2 = 20;
+console.log ("typeof undefined1, undefined 2")
+const myNull = null;
+console.log (typeof myNull)
+const myName = "Beqa Baindurashvili"
+console.log ("typeof myName")
+const myHobby = ("Gaming")
+console.log ("typeof myHobby")
+const yearOfBirth = 1999;
+console.log ("typeof yearOfBirth")
+const currentyear = 2023;
+console.log ("typeof currentYear")
+const substraction = currentyear - yearOfBirth;
+console.log (substraction)
+const str2 ="My name is"
+console.log ("typeof str2")
+const str3 ="I am"
+console.log ("typeof str3")
+const str4 = "years old"
+console.log ("typeof str4")
+const str5 = "my hobby is"
+console.log ("typeof str5")
+const add = str2 + myName + str3 + substraction + str4 + str5 + myHobby;
+console.log (add);
